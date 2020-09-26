3333abf62081a1111a1174d44b5d56f67358fede
Wilson Alves <wlsn.ads@gmail.com>
2020-09-26 00:30:24 -0300

    colocando campos user e senha

--- app/src/main/java/br/com/wilsonalves/opebarberware/MainActivity.kt
+++ app/src/main/java/br/com/wilsonalves/opebarberware/MainActivity.kt
@@ -2,10 +2,28 @@ package br.com.wilsonalves.opebarberware
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.widget.EditText
+import android.widget.Toast
+import kotlinx.android.synthetic.main.login.*
+import kotlinx.android.synthetic.main.login.textoInicial
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.login)
+
+
+        imgLogin.setImageResource(R.drawable.img_logo)
+        textoInicial.setText(R.string.mensagem_login)
+
+        botaoLogin.setOnClickListener{
+            val valorUsuario = campoUsuario.text.toString()
+            var valorSenha  = campoSenha.text.toString()
+            Toast.makeText(this, "Usuário $valorUsuario; Senha $valorSenha", Toast.LENGTH_LONG).show()
+        }
+
+
+        background.getBackground().setAlpha(128);
     }
 }
